4fcf925b957c269b948ccafc220370ab553dad52
Idan <idansano10@gmail.com>
2025-10-23 17:09:16 +0300

    added all function

---
+++ game.py
@@ -0,0 +1,73 @@
+from tabnanny import check
+
+from utils import *
+
+score_player_1 = 0
+score_player_2 = 0
+def play_game():
+    global score_player_1
+    global score_player_2
+    flag = True
+    while flag:
+        print("turn player 1:")
+        check_pass = 0
+        choose1 = turn_decision()
+        score_1_equal_100 = is_exact_100(score_player_1)
+        score_2_equal_100 = is_exact_100(score_player_2)
+        score_1_over_100 = is_bust(score_player_1)
+        score_2_over_100 = is_bust(score_player_2)
+        if check_pass == 2:
+            result = closer_to_target(score_player_1, score_player_2)
+            if result == None:
+                result_again = tie_breaker()
+                print(f"{result_again} won!!")
+                flag = False
+            print(result)
+            flag = False
+        # Checking if one of them has passed 100
+        if score_1_over_100 or score_2_over_100:
+            if score_1_over_100:
+                print("player 1 lose!! and player 2 won")
+            else:
+                print("player 2 lose!! and player 1 won")
+            flag = False
+        # Check, both of them reached 100
+        if score_1_equal_100 and score_2_equal_100:
+            status_win = tie_breaker()
+            print(f"{status_win} won!!")
+        # Checking if one of them reached exactly one hundred
+        if score_1_equal_100 or score_2_equal_100:
+            if score_1_equal_100:
+                print("player 1 won!!")
+            else:
+                check_pass += 1
+                print("player 2 won!!")
+            flag = False
+        # Starting the game execution and turns
+        if choose1 == "r":
+            if check_pass == 1:
+                check_pass -= 1
+            score_player1 = roll_two_d6()
+            print(score_player1)
+            score_player_1 += score_player1[0]
+            score_player_1 += score_player1[1]
+            print(f"Your total score: {score_player_1} ")
+        else:
+            print("turn player 2:")
+            choose2 = turn_decision()
+            if choose2 == "r":
+                if check_pass == 1:
+                    check_pass -= 1
+                score_player2 = roll_two_d6()
+                print(score_player2)
+                score_player_2 += score_player2[0]
+                score_player_2 += score_player2[1]
+                print(f"Your total score: {score_player_2}")
+            else:
+                check_pass += 1
+                continue
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,40 @@
+import random
+
+def roll_two_d6() -> tuple[int, int]:
+    roll_1 = random.randint(1, 6)
+    roll_2 = random.randint(1, 6)
+    return roll_1 , roll_2
+
+def is_bust(score: int) -> bool:
+    return score > 100
+
+def is_exact_100(score: int) -> bool:
+    return score == 100
+
+def closer_to_target(a: int, b: int, target: int = 100) -> int | None:
+    player_1 = target - a
+    player_2 = target - b
+    if player_1 > player_2:
+        return 2
+    elif player_2 > player_1:
+        return 1
+    return None
+
+def tie_breaker() -> int:
+    while True:
+        player1 = roll_two_d6()
+        player2 = roll_two_d6()
+        if player1 != player2:
+            break
+    if player1 > player2:
+        return 1
+    return 2
+
+
+def turn_decision() -> str:
+    choose = input("choose r for roll or p for pass: ")
+    if choose != "r" and choose != "p":
+        print("try again")
+        choose = input("choose r for roll or p for pass: ")
+    return choose
+
